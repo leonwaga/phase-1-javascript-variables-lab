813cfa2175025d43ee245f4d61449119b82b29dd
leon <leon.waga@student.moringaschool.comgit config --global user.name leongit config --global user.name leon>
2023-07-13 16:41:38 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution in this file!
+const companyName = 'Scuber';
+
+    function mostProfitableNeighborhood() { 
+        
+        let mostProfitableNeighborhood ="chelsea";}
+        
+        if (mostProfitableNeighborhood === "chelsea") {
+        return'chelsea';}
+
+    function companyCeo() {
+        
+        let companyCeo = 'susan smith';
+
+        if (companyCeo ==='susan smith') 
+        return('susan smith')};
